09ce99c5cf7f31ecf599510e743f8ca630a60cd7
HaydenBrP <hayden.poulter@iu-study.org>
2022-12-16 12:16:50 +0200

    added doc strings to my_site/views.py and urls.py

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-
+''' link url patterns to the views.py program'''
 urlpatterns = [
     path('', views.index),
 ]
--- my_site/views.py
+++ my_site/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+'''Return the index.html page to be displayed.
+'''
 # Create your views here.
 def index(request):
     return render(request, "index.html")
